cff063e0e50af18c3e8867570fdfca46a062b92a
ludwigjonsson93 <ludwigjonsson93@gmail.com>
2023-12-02 17:55:09 +0100

    Sloppy versions of day1 and day2 completed and added

---
+++ Day01/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Welcome to Day 1 of Advent of Code 2023");
+Console.WriteLine();
+
+string path = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory) + "/Input.txt";
+string[] lines = File.ReadAllLines(path);
+
+int solutionPart1 = part1(lines);
+
+Console.WriteLine($"The total sum of the calibration is {solutionPart1}");
+
+int solutionPart2 = part2(lines);
+
+Console.WriteLine($"The total sum of the calibration after new information is {solutionPart2}");
+
+Console.ReadLine();
+
+static int part1(string[] input){
+    int sumOfCalibrationValues = 0;
+    foreach(string line in input){
+        string calibrationString = string.Concat(line.Where(Char.IsDigit));
+        char[] linenumbers = new char[2];
+
+        linenumbers[0] = calibrationString[0];
+        linenumbers[1] = calibrationString[calibrationString.Length - 1];
+
+        int calibrationNumber = Int32.Parse(new string(linenumbers));
+
+        sumOfCalibrationValues += calibrationNumber;
+    }
+
+    return sumOfCalibrationValues;
+}
+
+static int part2(string[] input) {
+    int sumOfCalibrationValues = 0;
+
+    foreach(string line in input) {
+
+        string manipulatedString = line;
+        bool replace = true;
+        while (replace) {
+            if (manipulatedString.Contains("eightwo")) {
+                manipulatedString = manipulatedString.Replace("eightwo", "82");
+            }
+            else if (manipulatedString.Contains("twone")) {
+                manipulatedString = manipulatedString.Replace("twone", "21");
+            }
+            else if (manipulatedString.Contains("oneight")) {
+                manipulatedString = manipulatedString.Replace("oneight", "18");
+            }
+            else if (manipulatedString.Contains("one")) {
+                manipulatedString = manipulatedString.Replace("one", "1");
+            }
+            else if (manipulatedString.Contains("two")) {
+                manipulatedString = manipulatedString.Replace("two", "2");
+            }
+            else if (manipulatedString.Contains("three")) {
+                manipulatedString = manipulatedString.Replace("three", "3");
+            }
+            else if (manipulatedString.Contains("four")) {
+                manipulatedString = manipulatedString.Replace("four", "4");
+            }
+            else if (manipulatedString.Contains("five")) {
+                manipulatedString = manipulatedString.Replace("five", "5");
+            }
+            else if (manipulatedString.Contains("six")) {
+                manipulatedString = manipulatedString.Replace("six", "6");
+            }
+            else if (manipulatedString.Contains("seven")) {
+                manipulatedString = manipulatedString.Replace("seven", "7");
+            }
+            else if (manipulatedString.Contains("eight")) {
+                manipulatedString = manipulatedString.Replace("eight", "8");
+            }
+            else if (manipulatedString.Contains("nine")) {
+                manipulatedString = manipulatedString.Replace("nine", "9");
+            }
+            else
+                replace = false;
+        }
+
+        string calibrationString = string.Concat(manipulatedString.Where(Char.IsDigit));
+        char[] linenumbers = [calibrationString[0], calibrationString[calibrationString.Length - 1]];
+
+        int calibrationNumber = Int32.Parse(new string(linenumbers));
+
+        sumOfCalibrationValues += calibrationNumber;
+    }
+
+    return sumOfCalibrationValues;
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Welcome to Day 2 of Advent of Code 2023");
+Console.WriteLine();
+
+string path = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory) + "/Input.txt";
+string[] lines = File.ReadAllLines(path);
+
+int solutionPart1 = part1(lines);
+
+Console.WriteLine($"The total sum of the ID:s if possible games is {solutionPart1}");
+
+int solutionPart2 = part2(lines);
+
+Console.WriteLine($"The total sum of the power of the lowest amount of cubes is {solutionPart2}");
+
+Console.ReadLine();
+
+static int part1(string[] lines) {
+    int sumOfPossibleID = 0;
+
+    int redCubes = 12;
+    int greenCubes = 13;
+    int blueCubes = 14;
+
+    foreach (string line in lines) {
+        bool isPossible = true;
+        string[] firstSplitArray = line.Split(':');
+        int id = Int32.Parse(new string(firstSplitArray[0].Where(Char.IsDigit).ToArray()));
+        string[] secondSplitArray = firstSplitArray[1].Split(";");
+        foreach(string draw in secondSplitArray) {
+            string[] drawResult = draw.Split(",");
+            foreach(string result in drawResult) {
+                if (result.Contains("green")) {
+                    int greenresult = Int32.Parse(new string(result.Where(char.IsDigit).ToArray()));
+                    if(greenresult > greenCubes) {
+                        isPossible = false;
+                    }
+                } else if(result.Contains("blue")) {
+                    int blueresult = Int32.Parse(new string(result.Where(char.IsDigit).ToArray()));
+                    if(blueresult > blueCubes) {
+                        isPossible = false;
+                    }
+                } else {
+                    int redresult = Int32.Parse(new string(result.Where(char.IsDigit).ToArray()));
+                    if(redresult > redCubes) {
+                        isPossible = false;
+                    }
+                }
+            }
+        }
+
+        if(isPossible) {
+            sumOfPossibleID += id;
+        }
+    }
+    return sumOfPossibleID;
+}
+
+static int part2(string[] lines) {
+    int sumOfPowerFromGames = 0;
+
+    foreach (string line in lines) {
+        int highestBlue = 0, highestRed = 0, highestGreen = 0;
+        string[] firstSplitArray = line.Split(':');
+        string[] secondSplitArray = firstSplitArray[1].Split(";");
+        foreach (string draw in secondSplitArray) {
+            string[] drawResult = draw.Split(",");
+            foreach (string result in drawResult) {
+                if (result.Contains("green")) {
+                    int greenresult = Int32.Parse(new string(result.Where(char.IsDigit).ToArray()));
+                    if (greenresult > highestGreen) {
+                        highestGreen = greenresult;
+                    }
+                }
+                else if (result.Contains("blue")) {
+                    int blueresult = Int32.Parse(new string(result.Where(char.IsDigit).ToArray()));
+                    if (blueresult > highestBlue) {
+                        highestBlue = blueresult;
+                    }
+                }
+                else {
+                    int redresult = Int32.Parse(new string(result.Where(char.IsDigit).ToArray()));
+                    if (redresult > highestRed) {
+                        highestRed = redresult;
+                    }
+                }
+            }
+        }
+        int powerOfResults = (highestRed * highestGreen * highestBlue);
+        sumOfPowerFromGames += powerOfResults;
+    }
+    return sumOfPowerFromGames;
+}
